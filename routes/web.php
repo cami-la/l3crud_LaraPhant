7275a4ac99b088f982b088641ec74adb514c273d
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-03-13 12:20:13 -0300

    :construction: Display all customers | Laravel Livewire 3 CRUD
    Display all customers | Laravel Livewire 3 CRUD

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Livewire\CreateCustomer;
+use App\Livewire\Customers;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/customers/create', CreateCustomer::class);
+Route::get('/customers', Customers::class);
+
